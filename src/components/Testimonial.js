72cd40b0cfaaf5e519cc550fa2b9c9c4f7e499b8
Keyroombar <kirumbaaugustinewambugu@gmail.com>
2022-08-09 00:03:51 +0300

    fixing the testimonial mobile screens and also the titles

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -15,15 +15,7 @@ const Testimonial = () => {
               <img src={avatarImg1} alt="Paul Njoroge" />
             </div>
             <div className="testimonial-div-element-content">
-            It takes patience to learn something new, but it takes monumental
-              effort to keep on trying despite numerous failures, Augustine has
-              demonstrated both of the above, having been there to watch him
-              grow his skills has been one of the little pleasures of my life.
-              He is an excellent employee and an even better friend who has
-              demonstrated what sheer willpower is. Moreover, he has
-              consistently shown a willingness to learn and has continuously
-              delivered on his task in a timely manner, I couldn't ask for a
-              better friend.
+            Throughout the project, Augustine supported me and exceeded my expectations in every way. Our experience was personal and friendly. His ability to identify and present key requirements in an imaginative and creative way gave me confidence in him. Augustine understood our client's website needs better than we did. He complimented our vision perfectly so I would definitely recommend him highly enough. Without a doubt one of the most talented programmers out there. I always go back to Augustine. Smart, reliable, and professional. You won't be disappointed.
             </div>
             <div className="testimonial-div-element-person">
               <div className="testimonial-div-element-person-name">
